ec48e1049188f89d2455702d00f06b4429f68b28
Proc_Hey Ho <bruno.camargo@gotobiz.com.br>
2023-05-23 20:12:35 -0300

    Proj Parte 1 Barometro

---
+++ Barômetro/Barometro/Barrometro/Models/Humidade.cs
@@ -0,0 +1,23 @@
+﻿namespace Barrometro.Web.Models
+{
+    public class Humidade
+    {
+        public int IdHumidade { get; set; }
+        public string Cidade { get; set; }
+        public DateTime Data_dado { get; set; }
+        public Double Hum_atual { get; set; }
+        public Double Hum_max { get; set; }
+        public Double Hum_min { get; set; }
+
+
+        public static List<Humidade> ListaTemp()
+        {
+
+            List<Humidade> lst = new List<Humidade>();
+
+            lst.Add(new Humidade { IdHumidade = 12, Cidade = "Garibaldi", Data_dado = DateTime.Parse("2023-05-09"), Hum_atual = 68 , Hum_max = 75, Hum_min = 50});
+
+            return lst;
+        }
+    }
+}
